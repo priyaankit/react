06c74bed3807fc5a4a0110b9aba9b4b42b4e035c
priyaankit <52679910+priyaankit@users.noreply.github.com>
2022-05-15 18:39:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// Given a class component, add state to it
+// state should have a property called `isRaining` which is a boolean
+// (true if it is raining, false if not)
+// Render the word "Yes" if it is raining
+// or "No" if it is not raining
+
+import React from "react";
+
+class App extends React.Component {}
+
+export default App;
